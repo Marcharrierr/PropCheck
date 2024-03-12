986ac6e18114496005c9ba0271d78514945b0f5c
mcharrier1 <mcharrier@propcheck.ai>
2024-03-12 18:26:40 -0300

    Agregando carpeta dist

--- old_propcheck/src/app/home-page/home-page.component.ts
+++ old_propcheck/src/app/home-page/home-page.component.ts
@@ -6,7 +6,7 @@ import { Message } from 'primeng/api';
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']
+  Urls: ['./home-page.component.css']
 })
 export class HomePageComponent {
 
