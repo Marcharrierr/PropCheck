84c05a02a51520cf4ad263421afde1d528c60321
bioodev <bioodev@protonmail.com>
2024-03-15 13:21:24 -0300

    servicio de auth con consulta a api por agente y cliente, y nombre dinamico a navbar

--- src/app/component/sidebar/sidebar.component.ts
+++ src/app/component/sidebar/sidebar.component.ts
@@ -4,22 +4,28 @@ import { AuthService } from '../../services/auth.service';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent {
-
-  private authService = inject(AuthService)
-
-
-
+  private authService = inject(AuthService);
   sidebarVisible!: boolean;
+  
+  agentData: any;
+  clientData: any;
 
-  onLogout() {
+  ngOnInit(): void {
+    // Obtener los datos del cliente del localStorage
+    const agentStoredData = localStorage.getItem('agentData');
+    if (agentStoredData) {
+      this.agentData = JSON.parse(agentStoredData);
+    }
 
-    this.authService.logout();
-
+    const clientStoredData = localStorage.getItem('clientData');
+    if (clientStoredData) {
+      this.clientData = JSON.parse(clientStoredData);
+    }
   }
-
-
-
+  onLogout() {
+    this.authService.logout();
+  }
 }
