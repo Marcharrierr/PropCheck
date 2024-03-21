a70054824e0f924727420ff2439885cb76549352
mcharrier1 <mcharrier@propcheck.ai>
2024-03-21 11:26:55 -0300

    Cambios solución login e imagenes home clocalStorage

--- src/app/services/data-client.service.ts
+++ src/app/services/data-client.service.ts
@@ -9,21 +9,21 @@ export class DataClient {
   private baseUrl: string = 'https://api.propcheck.ai/propcheck/client';
 
   constructor(private http: HttpClient) { }
-  
+
   getClientId(): string | null {
     const agentDataString = localStorage.getItem('agentData');
-    
+
     if (agentDataString) {
       const agentData = JSON.parse(agentDataString);
-      return agentData.id;
+      return agentData.client_id;
     } else {
       return null;
     }
   }
-  
+
   getClient(): Observable<any> {
     const id = this.getClientId();
-    
+
     if (id) {
       const url = `${this.baseUrl}/${id}`;
       return this.http.get(url);
